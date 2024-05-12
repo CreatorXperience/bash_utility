d51ea2b780eefd5bbe230deaa1ca83ac18f073eb
CreatorXperience <allyearmustobey@gmail.com>
2024-05-12 10:59:43 +0000

    complete date bash script challenge

---
+++ datetime.s
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+print_date (){
+my_date="$(date +%4Y-%m-%d)"
+echo $my_date
+}
+
+print_date
---
+++ datetime.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+print_date (){
+my_date="$(date +%4Y-%m-%d\ %H:%M:%S) "
+echo $my_date
+}
+
+print_date
